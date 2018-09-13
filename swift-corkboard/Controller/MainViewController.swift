fa45057f8390881648d4b88100b9f5a1df3ebd27
jewell86 <jewellsmailbox@gmail.com>
2018-09-13 16:26:10 -0700

    work on populating boards

--- swift-corkboard/Controller/MainViewController.swift
+++ swift-corkboard/Controller/MainViewController.swift
@@ -12,32 +12,53 @@ import SwiftyJSON
 
 class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    let notes = [ "Hello there", "groceries", "todo list", "its me", "gagin", "fuck", "shit", "poop", "sexxxx" ]
-
-    //TODO: Set IBOutlets & IBActions
+    let notes = [ "Hello there", "groceries", "todo list", "its me", "again", "meme", "hay", "its a note", "poop" ]
+    
+    var boardArray : [BoardIcon] = [BoardIcon]()
+    
     @IBOutlet weak var allBoardsTableView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //TODO: Set delegates
-        allBoardsTableView.delegate = self
-        allBoardsTableView.dataSource = self
+    //TODO: Set delegates
+    allBoardsTableView.delegate = self
+    allBoardsTableView.dataSource = self
         
-        //Register NoteCell.xib file here
-        allBoardsTableView.register(UINib(nibName: "NoteCell", bundle: nil), forCellWithReuseIdentifier: "noteCell")
-        //Set size to auto layout
+    //TODO: Register NoteCell.xib file here
+    allBoardsTableView.register(UINib(nibName: "NoteCell", bundle: nil), forCellWithReuseIdentifier: "noteCell")
+    configureCollectionView()
+    renderBoards()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "noteCell", for: indexPath) as! NoteCell
+//        print(boardArray[indexPath.row].title)
+        
+        cell.noteTextInput.text = notes[indexPath.row]
+        return cell
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return notes.count
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func configureCollectionView() {
+        //TODO: Set size to auto layout
         if let flowLayout = allBoardsTableView.collectionViewLayout as? UICollectionViewFlowLayout {
             flowLayout.estimatedItemSize = CGSize(width: 100, height: 100)
         }
-        
         var isHeightCalculated: Bool = false
-        
         func preferredLayoutAttributesFittingAttributes(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
-            //Exhibit A - We need to cache our calculation to prevent a crash.
             if !isHeightCalculated {
-//                setNeedsLayout()
-//                layoutIfNeeded()
+                //                setNeedsLayout()
+                //                layoutIfNeeded()
                 let size = allBoardsTableView.systemLayoutSizeFitting(layoutAttributes.size)
                 var newFrame = layoutAttributes.frame
                 newFrame.size.width = CGFloat(ceilf(Float(size.width)))
@@ -46,65 +67,28 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
             }
             return layoutAttributes
         }
-        
-        //GET REQUEST FOR ALL BOARDS FOR USER
- 
-            Alamofire.request("http://localhost:5000/1/main", method: .get, encoding: JSONEncoding.default, headers: nil).responseJSON {
+    }
+    
+    func renderBoards() {
+        Alamofire.request("http://localhost:5000/1/main", method: .get, encoding: JSONEncoding.default, headers: nil).responseJSON {
             response in
             let data : JSON = JSON(response.result.value!)
-                print("DATA!!!!!!!")
-            print(data)
+            let allBoards = data["response"]
+            for board in allBoards.arrayValue {
+                let newBoard = BoardIcon()
+                newBoard.boards_id = board["boards_id"]
+                newBoard.added_by = board["added_by"]
+                newBoard.title = board["title"]
+                print(newBoard.title)
+                self.boardArray.append(newBoard)
             }
+            self.configureCollectionView()
+            self.allBoardsTableView.reloadData()
+        }
 
-//            let token = data["token"]
-//            let error = data["error"]
-//            print(token)
-//            if token == JSON.null {
-//                print("Error: \(error)")
-//            } else {
-//                print("Login Success!")
-//                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-//                let mainViewController = storyBoard.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
-//                self.present(mainViewController, animated: true, completion: nil)            }
-//            }
-
-        //populate buttons for each board w/ id#
-        //link each button to boardViewController
-        //make get request from boardViewController to get all board info
-    }
-    
-
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "noteCell", for: indexPath) as! NoteCell
-        
-        cell.noteTextInput.text = notes[indexPath.row]
-//        print(indexPath)
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return notes.count
     }
 
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
---
+++ swift-corkboard/Model/BoardIconModel.swift
@@ -0,0 +1,18 @@
+//
+//  BoardIconModel.swift
+//  swift-corkboard
+//
+//  Created by Jewell Braden on 9/13/18.
+//  Copyright © 2018 Jewell White. All rights reserved.
+//
+
+
+
+class BoardIcon{
+    
+    var boards_id : Any = ""
+    var added_by : Any = ""
+    var title : Any = ""
+}
+
+
